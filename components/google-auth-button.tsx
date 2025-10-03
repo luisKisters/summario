e738b8f1d8395d2f38bc58ac3b48b27c8ed344f2
luisKisters <luis.w.kisters@gmail.com>
2025-10-03 17:15:22 +0200

    fix(auth): renoved redirect

--- components/google-auth-button.tsx
+++ components/google-auth-button.tsx
@@ -7,9 +7,7 @@ import { useState } from "react";
 export function GoogleAuthButton() {
   const [isLoading, setIsLoading] = useState(false);
 
-  console.log(
-    `URL: ${process.env.NEXT_PUBLIC_APP_URL}/auth/callback?next=/meetings`
-  );
+  console.log(`URL: ${process.env.NEXT_PUBLIC_APP_URL}/auth/callback`);
 
   const handleGoogleSignIn = async () => {
     setIsLoading(true);
@@ -19,7 +17,7 @@ export function GoogleAuthButton() {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-          redirectTo: `${process.env.NEXT_PUBLIC_APP_URL}/auth/callback?next=/meetings`,
+          redirectTo: `${process.env.NEXT_PUBLIC_APP_URL}/auth/callback`,
         },
       });
 
